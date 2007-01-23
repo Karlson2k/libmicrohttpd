a84f9bc7b3896a197a1e4c41a5311d6b52928034
Christian Grothoff <christian@grothoff.org>
2007-01-23 05:48:21 +0000

    expanded testcases

--- src/daemon/daemon.c
+++ src/daemon/daemon.c
@@ -27,3 +27,240 @@
 #include "config.h"
 #include "microhttpd.h"
 
+
+/**
+ * Start a webserver on the given port.
+ * @param port port to bind to
+ * @param apc callback to call to check which clients
+ *        will be allowed to connect
+ * @param apc_cls extra argument to apc
+ * @param dh default handler for all URIs
+ * @param dh_cls extra argument to dh
+ * @return NULL on error, handle to daemon on success
+ */
+struct MHD_Daemon *
+MHD_start_daemon(unsigned int options,
+		 unsigned short port,
+		 MHD_AcceptPolicyCallback apc,
+		 void * apc_cls,
+		 MHD_AccessHandlerCallback dh,
+		 void * dh_cls) {
+  return NULL;
+}
+
+
+
+/**
+ * Shutdown an http daemon.
+ */
+void
+MHD_stop_daemon(struct MHD_Daemon * daemon) {
+}
+
+/**
+ * Obtain the select sets for this daemon.
+ *
+ * @return MHD_YES on success, MHD_NO if this
+ *         daemon was not started with the right
+ *         options for this call.
+ */
+int 
+MHD_get_fdset(struct MHD_Daemon * daemon,
+	      fd_set * read_fd_set,
+	      fd_set * write_fd_set,
+	      fd_set * except_fd_set,
+	      int * max_fd) {
+  return 0;
+}
+
+/**
+ * Run webserver operations (without blocking unless
+ * in client callbacks).  This method should be called
+ * by clients in combination with MHD_get_fdset
+ * if the client-controlled select method is used. 
+ *
+ * @return MHD_YES on success, MHD_NO if this
+ *         daemon was not started with the right
+ *         options for this call.
+ */
+int
+MHD_run(struct MHD_Daemon * daemon) {
+  return 0;
+}
+
+
+/**
+ * Register an access handler for all URIs beginning with uri_prefix.
+ *
+ * @param uri_prefix 
+ * @return MRI_NO if a handler for this exact prefix
+ *         already exists
+ */
+int 
+MHD_register_handler(struct MHD_Daemon * daemon,
+		     const char * uri_prefix,
+		     MHD_AccessHandlerCallback dh,
+		     void * dh_cls) {
+  return 0;
+}
+
+/**
+ * Unregister an access handler for the URIs beginning with
+ * uri_prefix.
+ *
+ * @param uri_prefix 
+ * @return MHD_NO if a handler for this exact prefix
+ *         is not known for this daemon
+ */
+int 
+MHD_unregister_handler(struct MHD_Daemon * daemon,
+		       const char * uri_prefix,
+		       MHD_AccessHandlerCallback dh,
+		       void * dh_cls) {
+  return 0;
+}
+
+/**
+ * Get all of the headers from the request.
+ *
+ * @param iterator callback to call on each header;
+ *        maybe NULL (then just count headers)
+ * @param iterator_cls extra argument to iterator
+ * @return number of entries iterated over
+ */ 
+int
+MHD_get_session_values(struct MHD_Session * session,
+		       enum MHD_ValueKind kind,
+		       MHD_KeyValueIterator * iterator,
+		       void * iterator_cls);
+
+/**
+ * Get a particular header value.  If multiple
+ * values match the kind, return any one of them.
+ *
+ * @param key the header to look for
+ * @return NULL if no such item was found
+ */ 
+const char *
+MHD_lookup_session_value(struct MHD_Session * session,
+			 enum MHD_ValueKind kind,
+			 const char * key) {
+  return NULL;
+}
+
+/**
+ * Queue a response to be transmitted to the client (as soon as
+ * possible).
+ * 
+ * @param session the session identifying the client
+ * @param status_code HTTP status code (i.e. 200 for OK)
+ * @param response response to transmit
+ * @return MHD_NO on error (i.e. reply already sent),
+ *         MHD_YES on success or if message has been queued
+ */
+int 
+MHD_queue_response(struct MHD_Session * session,
+		   unsigned int status_code,
+		   struct MHD_Response * response) {
+  return 0;
+}
+
+	       
+/**
+ * Create a response object.  The response object can be extended with
+ * header information and then be used any number of times.
+ *
+ * @param size size of the data portion of the response, -1 for unknown
+ * @param crc callback to use to obtain response data
+ * @param crc_cls extra argument to crc
+ * @param crfc callback to call to free crc_cls resources
+ * @return NULL on error (i.e. invalid arguments, out of memory)
+ */
+struct MHD_Response *
+MHD_create_response_from_callback(size_t size,
+				  MHD_ContentReaderCallback crc,
+				  void * crc_cls,
+				  MHD_ContentReaderFreeCallback crfc) {
+  return NULL;
+}
+
+/**
+ * Create a response object.  The response object can be extended with
+ * header information and then be used any number of times.
+ *
+ * @param size size of the data portion of the response
+ * @param data the data itself
+ * @param must_free libmicrohttpd should free data when done
+ * @param must_copy libmicrohttpd must make a copy of data 
+ *        right away, the data maybe released anytime after
+ *        this call returns
+ * @return NULL on error (i.e. invalid arguments, out of memory)
+ */
+struct MHD_Response *
+MHD_create_response_from_data(size_t size,
+			      void * data,
+			      int must_free,
+			      int must_copy) {
+  return NULL;
+}
+
+/**
+ * Destroy a response object and associated resources.  Note that
+ * libmicrohttpd may keep some of the resources around if the response
+ * is still in the queue for some clients, so the memory may not
+ * necessarily be freed immediatley.
+ */
+void
+MHD_destroy_response(struct MHD_Response * response) {
+}
+
+/**
+ * Add a header line to the response.
+ *
+ * @return MHD_NO on error (i.e. invalid header or content format).
+ */
+int
+MHD_add_response_header(struct MHD_Response * response,
+			const char * header,
+			const char * content) {
+  return 0;
+}
+
+/**
+ * Delete a header line from the response.
+ *
+ * @return MHD_NO on error (no such header known)
+ */
+int
+MHD_del_response_header(struct MHD_Response * response,
+			const char * header,
+			const char * content) {
+  return 0;
+}
+
+/**
+ * Get all of the headers added to a response.
+ *
+ * @param iterator callback to call on each header;
+ *        maybe NULL (then just count headers)
+ * @param iterator_cls extra argument to iterator
+ * @return number of entries iterated over
+ */ 
+int
+MHD_get_response_headers(struct MHD_Response * response,
+			 MHD_KeyValueIterator * iterator,
+			 void * iterator_cls) {
+  return -1;
+}
+
+/**
+ * @return -1 if no data uploaded; otherwise number of bytes
+ *            read into buf; 0 for end of transmission
+ */
+int 
+MHD_read_file_upload(struct MHD_Session * session,
+		     void * buf,
+		     size_t len) {
+  return -1;
+}
+
--- src/daemon/daemontest.c
+++ src/daemon/daemontest.c
@@ -1,6 +1,6 @@
 /*
      This file is part of libmicrohttpd
-     (C) 2007 YOUR NAME HERE
+     (C) 2007 Christian Grothoff
 
      libmicrohttpd is free software; you can redistribute it and/or modify
      it under the terms of the GNU General Public License as published
@@ -21,14 +21,156 @@
 /**
  * @file daemontest.c
  * @brief  Testcase for libmicrohttpd
- * @author FIXME
+ * @author Christian Grothoff
  */
 
 #include "config.h"
+#include "curl/curl.h"
 #include "microhttpd.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
+static int testStartError() {
+  return NULL != MHD_start_daemon(0, 0, NULL, NULL, NULL, NULL);
+}
+
+static int apc_nothing(void * cls,
+		       const struct sockaddr * addr,
+		       socklen_t addrlen) {
+  return MHD_NO;
+}
+
+static int apc_all(void * cls,
+		   const struct sockaddr * addr,
+		   socklen_t addrlen) {
+  return MHD_YES;
+}
+
+static int ahc_nothing(void * cls,
+		       struct MHD_Session * session,
+		       const char * url,
+		       const char * method) {
+  return MHD_NO;
+}
+
+struct CBC {
+  char * buf;
+  size_t pos;
+  size_t size;
+};
+
+static size_t copyBuffer(void * ptr,
+			 size_t size,
+			 size_t nmemb,
+			 void * ctx) {
+  struct CBC * cbc = ctx;
+
+  if (cbc->pos + size * nmemb > cbc->size)
+    return 0; /* overflow */
+  memcpy(&cbc->buf[cbc->pos],
+	 ptr,
+	 size * nmemb);
+  cbc->pos += size * nmemb;
+  return size * nmemb;
+}
+
+static int ahc_echo(void * cls,
+		    struct MHD_Session * session,
+		    const char * url,
+		    const char * method) {
+  const char * me = cls;
+  struct MHD_Response * response;
+  int ret;
+
+  if (0 != strcmp(me, method))
+    return MHD_NO; /* unexpected method */
+  response = MHD_create_response_from_data(strlen(url),
+					   (void*) url,
+					   MHD_NO,
+					   MHD_YES);
+  ret = MHD_queue_response(session,
+			   MHD_HTTP_OK,
+			   response);
+  MHD_destroy_response(response);
+  return ret;
+}
+
+static int testStartStop() {
+  struct MHD_Daemon * d;
+
+  d = MHD_start_daemon(MHD_USE_SELECT_INTERNALLY | MHD_USE_IPv4,
+		       1080,
+		       &apc_nothing,
+		       NULL,
+		       &ahc_nothing,
+		       NULL);
+  if (d == NULL)
+    return 1;
+  MHD_stop_daemon(d);
+  return 0;
+}
+
+static int testGet() {
+  struct MHD_Daemon * d;
+  CURL * c;
+  int ret;
+  char buf[2048];
+  struct CBC cbc;
+
+  cbc.buf = buf;
+  cbc.size = 2048;
+  cbc.pos = 0;
+  ret = 0;
+  d = MHD_start_daemon(MHD_USE_SELECT_INTERNALLY | MHD_USE_IPv4,
+		       1080,
+		       &apc_all,
+		       NULL,
+		       &ahc_echo,
+		       "GET");
+  if (d == NULL)
+    return 1;
+  c = curl_easy_init();
+  curl_easy_setopt(c,
+		   CURLOPT_URL,
+		   "http://localhost:1080/hello_world");
+  curl_easy_setopt(c,
+		   CURLOPT_WRITEFUNCTION,
+		   &copyBuffer);
+  curl_easy_setopt(c,
+		   CURLOPT_WRITEDATA,
+		   &cbc);
+  curl_easy_setopt(c,
+		   CURLOPT_FAILONERROR,
+		   1);
+  curl_easy_setopt(c,
+		   CURLOPT_CONNECTTIMEOUT,
+		   15L);
+  /* NOTE: use of CONNECTTIMEOUT without also
+     setting NOSIGNAL results in really weird
+     crashes on my system! */
+  curl_easy_setopt(c,
+		   CURLOPT_NOSIGNAL,
+		   1);  
+  if (CURLE_OK != curl_easy_perform(c))
+    ret = 1;
+  curl_easy_cleanup(c);
+  if (cbc.pos != strlen("hello_world"))
+    ret = 2;
+  if (0 != strncmp("hello_world",
+		   cbc.buf,
+		   strlen("hello_world")))
+    ret = 3;
+  MHD_stop_daemon(d);
+  return ret;
+}
 
 int main(int argc,
 	 char * const * argv) {
-  return 0; /* 0 == pass */
+  unsigned int errorCount = 0;
+  
+  errorCount += testStartError();
+  errorCount += testStartStop();
+  errorCount += testGet();
+  return errorCount != 0; /* 0 == pass */
 }
