ce0473d52411d1bb274ae21b237f5af6f82c5134
Christian Grothoff <christian@grothoff.org>
2008-12-27 07:33:04 +0000

    better script

--- contrib/coverage.sh
+++ contrib/coverage.sh
@@ -2,6 +2,12 @@
 # make sure configure was run with coverage enabled...
 lcov --directory . --zerocounters
 make check
+for n in `find * -name "*.gc*" | grep libs`
+do
+  cd `dirname $n`
+  mv `basename $n` ..
+  cd -
+done
 lcov --directory . --capture --output-file app.info
 mkdir /tmp/coverage
 genhtml -o /tmp/coverage app.info
