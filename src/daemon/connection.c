0f7b49b524f9adfcf6f71e64d9b76b1be9f96f70
Christian Grothoff <christian@grothoff.org>
2010-11-18 21:56:42 +0000

    patch as suggested in 1621

--- src/daemon/connection.c
+++ src/daemon/connection.c
@@ -1456,7 +1456,7 @@ do_read (struct MHD_Connection *connection)
       /* other side closed connection */
       connection->read_closed = MHD_YES;
       SHUTDOWN (connection->socket_fd, SHUT_RD);
-      return MHD_NO;
+      return MHD_YES;
     }
   connection->read_buffer_offset += bytes_read;
   return MHD_YES;
