a84334b18d15984b881f8eace7149c5ee9581837
Christian Grothoff <christian@grothoff.org>
2011-06-22 08:28:49 +0000

    docu

--- src/include/microhttpd.h
+++ src/include/microhttpd.h
@@ -1083,10 +1083,11 @@ MHD_get_fdset (struct MHD_Daemon *daemon,
                fd_set * write_fd_set, fd_set * except_fd_set, int *max_fd);
 
 /**
- * Obtain timeout value for select for this daemon
- * (only needed if connection timeout is used).  The
- * returned value is how long select should at most
- * block, not the timeout value set for connections.
+ * Obtain timeout value for select for this daemon (only needed if
+ * connection timeout is used).  The returned value is how long select
+ * should at most block, not the timeout value set for connections.
+ * This function MUST NOT be called if MHD is running with
+ * "MHD_USE_THREAD_PER_CONNECTION".
  *
  * @param daemon daemon to query for timeout
  * @param timeout set to the timeout (in milliseconds)
