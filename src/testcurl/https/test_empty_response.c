26ed57fcf987edd4c641477d52e2f9d386bedf43
Christian Grothoff <christian@grothoff.org>
2013-03-04 13:42:40 +0000

    testcase for #2783

--- src/testcurl/https/test_empty_response.c
+++ src/testcurl/https/test_empty_response.c
@@ -1,10 +1,10 @@
 /*
  This file is part of libmicrohttpd
- (C) 2007 Christian Grothoff
+ (C) 2013 Christian Grothoff
 
  libmicrohttpd is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published
- by the Free Software Foundation; either version 2, or (at your
+ by the Free Software Foundation; either version 3, or (at your
  option) any later version.
 
  libmicrohttpd is distributed in the hope that it will be useful, but
@@ -19,11 +19,10 @@
  */
 
 /**
- * @file mhds_get_test.c
- * @brief  Testcase for libmicrohttpd HTTPS GET operations
- * @author Sagie Amir
+ * @file test_empty_response.c
+ * @brief  Testcase for libmicrohttpd HTTPS GET operations with emtpy reply
+ * @author Christian Grothoff
  */
-
 #include "platform.h"
 #include "microhttpd.h"
 #include <limits.h>
@@ -48,31 +47,19 @@ ahc_echo (void *cls,
           const char *upload_data, size_t *upload_data_size,
           void **unused)
 {
-  static int ptr;
-  const char *me = cls;
   struct MHD_Response *response;
   int ret;
 
-  if (0 != strcmp (me, method))
-    return MHD_NO;              /* unexpected method */
-  if (&ptr != *unused)
-    {
-      *unused = &ptr;
-      return MHD_YES;
-    }
-  *unused = NULL;
-  response = MHD_create_response_from_buffer (strlen (url),
-					      (void *) url,
-					      MHD_RESPMEM_MUST_COPY);
+  response = MHD_create_response_from_buffer (0, NULL,
+					      MHD_RESPMEM_PERSISTENT);
   ret = MHD_queue_response (connection, MHD_HTTP_OK, response);
   MHD_destroy_response (response);
-  if (ret == MHD_NO)
-    abort ();
   return ret;
 }
 
+
 static int
-testExternalGet ()
+testInternalSelectGet ()
 {
   struct MHD_Daemon *d;
   CURL *c;
@@ -93,7 +80,7 @@ testExternalGet ()
   cbc.buf = buf;
   cbc.size = 2048;
   cbc.pos = 0;
-  d = MHD_start_daemon (MHD_USE_DEBUG | MHD_USE_SSL,
+  d = MHD_start_daemon (MHD_USE_DEBUG | MHD_USE_SSL | MHD_USE_SELECT_INTERNALLY,
                         1082, NULL, NULL, &ahc_echo, "GET", 
                         MHD_OPTION_HTTPS_MEM_KEY, srv_key_pem,
                         MHD_OPTION_HTTPS_MEM_CERT, srv_self_signed_cert_pem,
@@ -160,14 +147,6 @@ testExternalGet ()
           MHD_stop_daemon (d);
           return 2048;
         }
-      if (MHD_YES != MHD_get_fdset (d, &rs, &ws, &es, &max))
-        {
-          curl_multi_remove_handle (multi, c);
-          curl_multi_cleanup (multi);
-          curl_easy_cleanup (c);
-          MHD_stop_daemon (d);
-          return 4096;
-        }
       tv.tv_sec = 0;
       tv.tv_usec = 1000;
       select (max + 1, &rs, &ws, &es, &tv);
@@ -191,7 +170,6 @@ testExternalGet ()
               multi = NULL;
             }
         }
-      MHD_run (d);
     }
   if (multi != NULL)
     {
@@ -200,10 +178,8 @@ testExternalGet ()
       curl_multi_cleanup (multi);
     }
   MHD_stop_daemon (d);
-  if (cbc.pos != strlen ("/hello_world"))
+  if (cbc.pos != 0)
     return 8192;
-  if (0 != strncmp ("/hello_world", cbc.buf, strlen ("/hello_world")))
-    return 16384;
   return 0;
 }
 
@@ -222,7 +198,7 @@ main (int argc, char *const *argv)
       fprintf (stderr, "Error: %s\n", strerror (errno));
       return -1;
     }
-  if (0 != (errorCount = testExternalGet ()))
+  if (0 != (errorCount = testInternalSelectGet ()))
     fprintf (stderr, "Fail: %d\n", errorCount);
   curl_global_cleanup ();
   return errorCount != 0;
