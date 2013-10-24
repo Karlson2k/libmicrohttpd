a79c58be3d55fb21d9d47bd6f7a3b5367ad70569
Christian Grothoff <christian@grothoff.org>
2013-10-24 08:39:55 +0000

    -forgotten commit for 0.9.31 release

--- src/include/microhttpd.h
+++ src/include/microhttpd.h
@@ -121,7 +121,7 @@ extern "C"
  * Current version of the library.
  * 0x01093001 = 1.9.30-1.
  */
-#define MHD_VERSION 0x00093002
+#define MHD_VERSION 0x00093100
 
 /**
  * MHD-internal return code for "YES".
