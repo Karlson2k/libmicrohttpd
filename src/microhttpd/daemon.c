583dc11b22fd086a389399a019363be7f44c70fe
Evgeny Grin (Karlson2k) <k2k@narod.ru>
2014-04-01 06:22:45 +0000

    Really check for presence of pthread_setname_np() instead of guessing

--- src/microhttpd/daemon.c
+++ src/microhttpd/daemon.c
@@ -1073,11 +1073,9 @@ create_thread (pthread_t *thread,
     }
   ret = pthread_create (thread, pattr,
 			start_routine, arg);
-#if (__GLIBC__ == 2) && (__GLIBC_MINOR__ >= 12)
-#if LINUX
+#ifdef HAVE_PTHREAD_SETNAME_NP
   (void) pthread_setname_np (*thread, "libmicrohttpd");
-#endif
-#endif
+#endif /* HAVE_PTHREAD_SETNAME_NP */
   if (0 != daemon->thread_stack_size)
     pthread_attr_destroy (&attr);
   return ret;
