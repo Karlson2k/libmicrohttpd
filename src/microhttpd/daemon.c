cfd8cfeefaf5b60fc383dd376263f86305f6c4ee
Evgeny Grin (Karlson2k) <k2k@narod.ru>
2016-10-28 22:22:46 +0300

    Fixed regression introduced by dd327c1d13252373cd1730e445d19930417571bf

--- src/microhttpd/daemon.c
+++ src/microhttpd/daemon.c
@@ -2478,6 +2478,7 @@ resume_suspended_connections (struct MHD_Daemon *daemon)
   if (0 != (daemon->options & MHD_USE_THREAD_PER_CONNECTION))
     MHD_mutex_lock_chk_ (&daemon->cleanup_connection_mutex);
 
+  next = daemon->suspended_connections_head;
   /* Clear the flag *only* if connections will be resumed otherwise
      it may accidentally clear flag that was set at the same time in
      other thread (just after 'if (MHD_NO != daemon->resuming)' in
