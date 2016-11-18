816458fde97a41082796eb8d3323047cc4f87935
Christian Grothoff <christian@grothoff.org>
2016-11-18 16:48:15 +0100

    give a bit more context in log message

--- src/microhttpd/connection.c
+++ src/microhttpd/connection.c
@@ -2588,7 +2588,8 @@ MHD_connection_handle_write (struct MHD_Connection *connection)
                   return MHD_YES;
 #ifdef HAVE_MESSAGES
                 MHD_DLOG (connection->daemon,
-                          _("Failed to send data: %s\n"),
+                          _("Failed to send data in request for `%s': %s\n"),
+                          connection->url,
                           MHD_socket_strerr_ (err));
 #endif
                 CONNECTION_CLOSE_ERROR (connection,
