Karlson2k/libmicrohttpd

commit f3b0eb539e8a0e87745dc53eabe507b98f1864b6

fix build issue if HAVE_MESSAGES is not set

src/microhttpd/connection.c
@@ -3111,6 +3111,15 @@ MHD_connection_handle_idle (struct MHD_Connection *connection)
           /* no default action */
           break;
         case MHD_CONNECTION_FOOTERS_SENT:
+	  if (MHD_HTTP_PROCESSING == connection->response->status_code)
+	  {
+	    /* After this type of response, we allow sending another! */
+	    connection->state = MHD_CONNECTION_HEADERS_PROCESSED;
+	    MHD_destroy_response (connection->response);
+	    connection->response = NULL;
+	    /* FIXME: maybe partially reset memory pool? */
+	    continue;
+	  }
           if (MHD_NO != socket_flush_possible (connection))
             socket_start_no_buffering_flush (connection);
           else

src/microhttpd/daemon.c
@@ -2541,10 +2541,12 @@ resume_suspended_connections (struct MHD_Daemon *daemon)
     { /* Wake up suspended connections. */
       if (! MHD_itc_activate_(daemon->itc,
                               "w"))
+	{
 #ifdef HAVE_MESSAGES
-        MHD_DLOG (daemon,
-                  _("Failed to signal resume of connection via inter-thread communication channel."));
+	  MHD_DLOG (daemon,
+		    _("Failed to signal resume of connection via inter-thread communication channel."));
 #endif
+	}
     }
   return ret;
 }

commit eee0b9ad40d13311b989cfc9995df53fba960d94

Fixed regression introduced by f3b0eb539e8a0e87745dc53eabe507b98f1864b6

src/microhttpd/connection.c
@@ -3111,7 +3111,7 @@ MHD_connection_handle_idle (struct MHD_Connection *connection)
           /* no default action */
           break;
         case MHD_CONNECTION_FOOTERS_SENT:
-	  if (MHD_HTTP_PROCESSING == connection->response->status_code)
+	  if (MHD_HTTP_PROCESSING == connection->responseCode)
 	  {
 	    /* After this type of response, we allow sending another! */
 	    connection->state = MHD_CONNECTION_HEADERS_PROCESSED;