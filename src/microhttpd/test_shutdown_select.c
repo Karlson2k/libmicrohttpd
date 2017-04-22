10a80118c345e4517cb6564f91e234c988cafccc
Evgeny Grin (Karlson2k) <k2k@narod.ru>
2017-04-22 15:24:58 +0300

    SQ makefile

--- src/microhttpd/test_shutdown_select.c
+++ src/microhttpd/test_shutdown_select.c
@@ -32,7 +32,7 @@
 #include "MHD_config.h"
 #if defined(MHD_USE_SOLARIS_THREADS) && defined(MHD_USE_POSIX_THREADS)
 #  undef MHD_USE_SOLARIS_THREADS
-#endif
+#endif /* MHD_USE_SOLARIS_THREADS && MHD_USE_POSIX_THREADS */
 #include "platform.h"
 #include "mhd_sockets.h"
 #include <stdlib.h>
