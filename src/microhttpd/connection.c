Karlson2k/libmicrohttpd

commit e264d9ec9ef98e6f26f5f547ec941927a35ffa56

Fixed response leak when upgrade failed

src/microhttpd/connection.c
@@ -3161,14 +3161,11 @@ MHD_connection_handle_idle (struct MHD_Connection *connection)
 #ifdef UPGRADE_SUPPORT
           if (NULL != connection->response->upgrade_handler)
             {
-              struct MHD_Response *resp_clr = connection->response;
-
               socket_start_normal_buffering (connection);
               connection->state = MHD_CONNECTION_UPGRADE;
-              connection->response = NULL;
               /* This connection is "upgraded".  Pass socket to application. */
               if (MHD_YES !=
-                  MHD_response_execute_upgrade_ (resp_clr,
+                  MHD_response_execute_upgrade_ (connection->response,
                                                  connection))
                 {
                   /* upgrade failed, fail hard */
@@ -3177,8 +3174,8 @@ MHD_connection_handle_idle (struct MHD_Connection *connection)
                   continue;
                 }
               /* Response is not required anymore for this connection. */
-              if (NULL != resp_clr)
-                MHD_destroy_response (resp_clr);
+              if (NULL != connection->response)
+                MHD_destroy_response (connection->response);
               continue;
             }
 #endif /* UPGRADE_SUPPORT */

commit 28ca87140129b554b5d77b55f422bddb8880b40a

Fixed response cleanup regression introduced by e264d9ec9ef98e6f26f5f547ec941927a35ffa56

src/microhttpd/connection.c
@@ -3175,7 +3175,11 @@ MHD_connection_handle_idle (struct MHD_Connection *connection)
                 }
               /* Response is not required anymore for this connection. */
               if (NULL != connection->response)
-                MHD_destroy_response (connection->response);
+                {
+                  struct MHD_Response const *resp = connection->response;
+                  connection->response = NULL;
+                  MHD_destroy_response (resp);
+                }
               continue;
             }
 #endif /* UPGRADE_SUPPORT */