a3e227ea1590bc2dc3c46bd708b842d4d3818828
Evgeny Grin (Karlson2k) <k2k@narod.ru>
2017-10-30 18:21:51 +0300

    https test: fixed too long waiting for empty fdsets

--- src/testcurl/https/test_empty_response.c
+++ src/testcurl/https/test_empty_response.c
@@ -181,7 +181,7 @@ testInternalSelectGet ()
             }
         }
       else
-        sleep (1000);
+        sleep (1);
       curl_multi_perform (multi, &running);
       if (running == 0)
         {
--- src/testcurl/https/test_https_get_select.c
+++ src/testcurl/https/test_https_get_select.c
@@ -207,7 +207,7 @@ testExternalGet (int flags)
             }
         }
       else
-        sleep (1000);
+        sleep (1);
       curl_multi_perform (multi, &running);
       if (running == 0)
         {
