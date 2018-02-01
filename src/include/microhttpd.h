21b2dd96aaffa8e2d0937a339bbdace882db0fee
Christian Grothoff <christian@grothoff.org>
2018-02-01 10:18:02 +0100

    getting ready for 0.9.59

--- src/include/microhttpd.h
+++ src/include/microhttpd.h
@@ -126,7 +126,7 @@ typedef intptr_t ssize_t;
  * Current version of the library.
  * 0x01093001 = 1.9.30-1.
  */
-#define MHD_VERSION 0x00095801
+#define MHD_VERSION 0x00095900
 
 /**
  * MHD-internal return code for "YES".
