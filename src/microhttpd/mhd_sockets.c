18a607a51883dc185a8150f0a4ca4d6ada9777c7
Christian Grothoff <christian@grothoff.org>
2018-12-08 23:18:08 +0100

    use WSA_FLAG_OVERLAPPED in W32 sockets as per suggestion from Johnathan McDougall on the mailinglist

--- src/microhttpd/mhd_sockets.c
+++ src/microhttpd/mhd_sockets.c
@@ -484,7 +484,7 @@ MHD_socket_create_listen_ (int pf)
                    0,
                    NULL,
                    0,
-                   WSA_FLAG_NO_HANDLE_INHERIT);
+                   WSA_FLAG_OVERLAPPED | WSA_FLAG_NO_HANDLE_INHERIT);
   cloexec_set = !0;
 #else  /* !SOCK_CLOEXEC */
   fd = MHD_INVALID_SOCKET;
