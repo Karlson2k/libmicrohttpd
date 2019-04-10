8b8c300312dc33a6c9cef232fb8710cb95723772
Evgeny Grin (Karlson2k) <k2k@narod.ru>
2019-04-10 14:31:16 +0300

    bootstrap: improved shell compatibility, removed unneeded 'aclocal' execution, removed '-f' autoreconf argument, added wrapper script autogen.sh

---
+++ autogen.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# This file was added for compatibility with some automated build systems.
+# It is recommended to use 'bootstrap' directly instead.
+
+ag_srcdir="${0%/*}" && ag_srcdir="${ag_srcdir}${ag_srcdir:+/}"
+"${ag_srcdir}bootstrap" ${1+"$@"}
