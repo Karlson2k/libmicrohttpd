2754ba51f406c2d8a14f689c397c6982568bc06b
Christian Grothoff <christian@grothoff.org>
2019-08-01 21:41:35 +0200

    do it in both tests

--- src/microhttpd/test_upgrade_large.c
+++ src/microhttpd/test_upgrade_large.c
@@ -705,6 +705,8 @@ run_usock (void *cls)
 {
   struct MHD_UpgradeResponseHandle *urh = cls;
 
+  MHD_upgrade_action (urh,
+                      MHD_UPGRADE_ACTION_CORK_OFF);
   send_all (usock,
             LARGE_STRING);
   recv_all (usock,
