13d3805dc696c2b72826ba862ed1347f17634510
Christian Grothoff <christian@grothoff.org>
2019-10-24 17:48:14 +0200

    use bit-test as we do in MHD_get_connection_values()

--- src/microhttpd/connection.c
+++ src/microhttpd/connection.c
@@ -350,12 +350,12 @@ MHD_get_connection_values_n (struct MHD_Connection *connection,
   if (NULL == iterator)
     for (pos = connection->headers_received; NULL != pos; pos = pos->next)
     {
-      if (kind == pos->kind)
+      if (0 != (kind & pos->kind))
         ret++;
     }
   else
     for (pos = connection->headers_received; NULL != pos; pos = pos->next)
-      if (kind == pos->kind)
+      if (0 != (kind & pos->kind))
       {
         ret++;
         if (MHD_NO == iterator (iterator_cls,
