Karlson2k/libmicrohttpd

commit 3b435274a2b3cd206201cc6140f2c70016a03bd2

Simplified call of MHD_send_on_connection2_()

src/microhttpd/connection.c
@@ -2917,25 +2917,18 @@ MHD_connection_handle_write (struct MHD_Connection *connection)
                               - connection->write_buffer_send_offset;
       mhd_assert (connection->write_buffer_append_offset >= \
                   connection->write_buffer_send_offset);
+      mhd_assert (NULL != connection->response);
+      mhd_assert ( (0 == connection->response->data_size) || \
+                   (0 == connection->response->data_start) );
 
-      /* if the response body is not available, we use MHD_send_on_connection_() */
-      if (NULL != connection->response->crc)
-      {
-        ret = MHD_send_on_connection_ (connection,
-                                       &connection->write_buffer
-                                       [connection->write_buffer_send_offset],
-                                       wb_ready,
-                                       MHD_SSO_PREFER_BUFF);
-      }
-      else
-      {
-        ret = MHD_send_on_connection2_ (connection,
-                                        &connection->write_buffer
-                                        [connection->write_buffer_send_offset],
-                                        wb_ready,
-                                        connection->response->data,
-                                        connection->response->data_size);
-      }
+      /* Send response headers alongside the response body, if the body
+       * data is available. */
+      ret = MHD_send_on_connection2_ (connection,
+                                      &connection->write_buffer
+                                      [connection->write_buffer_send_offset],
+                                      wb_ready,
+                                      connection->response->data,
+                                      connection->response->data_size);
 
       if (ret < 0)
       {
@@ -2949,12 +2942,11 @@ MHD_connection_handle_write (struct MHD_Connection *connection)
       /* 'ret' is not negative, it's safe to cast it to 'size_t'. */
       if (((size_t) ret) > wb_ready)
       {
-        mhd_assert (NULL == connection->response->crc);
-        /* We sent not just header data but also some response data,
-           update both offsets! */
+        /* The complete header and some response data have been sent,
+         * update both offsets. */
+        mhd_assert (! connection->have_chunked_upload);
         connection->write_buffer_send_offset += wb_ready;
-        ret -= wb_ready;
-        connection->response_write_position += ret;
+        connection->response_write_position += ret - wb_ready;
       }
       else
         connection->write_buffer_send_offset += ret;

commit 9c70bc55517cc61804a1d88540305d0477557eb6

Do not send dynamic response body with header
Fixed regression introduced by 3b435274a2b3cd206201cc6140f2c70016a03bd2
Do not use response dynamic data while responses have
connection-specific data and need to be locked

src/microhttpd/connection.c
@@ -2919,14 +2919,16 @@ MHD_connection_handle_write (struct MHD_Connection *connection)
                   connection->write_buffer_send_offset);
       mhd_assert (NULL != connection->response);
       mhd_assert ( (0 == connection->response->data_size) || \
-                   (0 == connection->response->data_start) );
+                   (0 == connection->response->data_start) || \
+                   (NULL != connection->response->crc) );
       mhd_assert ( (0 == connection->response_write_position) || \
                    (response->total_size ==
                     connection->response_write_position) || \
                    (MHD_SIZE_UNKNOWN ==
                     connection->response_write_position) );
 
-      if (0 == connection->response_write_position)
+      if ( (NULL == connection->response->crc) &&
+           (0 == connection->response_write_position) )
       {
         /* Send response headers alongside the response body, if the body
          * data is available. */
@@ -2939,8 +2941,8 @@ MHD_connection_handle_write (struct MHD_Connection *connection)
       }
       else
       {
-        /* This is response for HEAD request or reply body is not allowed
-         * for any other reason. */
+        /* This is response for HEAD request, reply body is not allowed
+         * for any other reason or reply body is dynamically generated. */
         /* Do not send the body data even if it's available. */
         ret = MHD_send_on_connection2_ (connection,
                                         &connection->write_buffer