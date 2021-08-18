ebd43f0373e3b62bf25213b5cd20a84e09c010e3
Christian Grothoff <christian@grothoff.org>
2021-08-18 11:10:42 +0200

    -bump version

--- src/include/microhttpd.h
+++ src/include/microhttpd.h
@@ -131,7 +131,7 @@ typedef intptr_t ssize_t;
  * they are parsed as decimal numbers.
  * Example: 0x01093001 = 1.9.30-1.
  */
-#define MHD_VERSION 0x00097303
+#define MHD_VERSION 0x00097304
 
 
 #ifdef __has_attribute
