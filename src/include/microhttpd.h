81daa705ca9bc8bec6cf877c684021cabddcca61
Evgeny Grin (Karlson2k) <k2k@narod.ru>
2022-04-19 19:58:31 +0300

    microhttpd.h: minor doxy improvement

--- src/include/microhttpd.h
+++ src/include/microhttpd.h
@@ -3499,6 +3499,10 @@ enum MHD_ResponseMemoryMode
    * Buffer is heap-allocated with `malloc()` (or equivalent) and
    * should be freed by MHD after processing the response has
    * concluded (response reference counter reaches zero).
+   * @warning Make sure that your application and MHD are using the same
+   *          C-runtime library (especially important for W32). if in doubt,
+   *          use function MHD_create_response_from_buffer_with_free_callback()
+   *          with '&free' as crfc parameter.
    * @ingroup response
    */
   MHD_RESPMEM_MUST_FREE,
