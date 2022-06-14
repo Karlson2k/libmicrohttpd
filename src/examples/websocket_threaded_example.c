95d163cc84e0be7a2198b5a00d48299e2c683f02
Evgeny Grin (Karlson2k) <k2k@narod.ru>
2022-06-14 20:13:46 +0300

    websocket_threaded_example: minor fixes

--- src/examples/websocket_threaded_example.c
+++ src/examples/websocket_threaded_example.c
@@ -280,7 +280,7 @@ SHA1Result (struct SHA1Context *context, unsigned char
   }
   if (context->corrupted)
   {
-    return context->corrupted;
+    return SHA1_RESULT_STATE_ERROR;
   }
   if (! context->computed)
   {
@@ -316,12 +316,12 @@ SHA1Input (struct SHA1Context *context, const unsigned char *message_array,
   }
   if (context->computed)
   {
-    context->corrupted = SHA1_RESULT_STATE_ERROR;
+    context->corrupted = 1;
     return SHA1_RESULT_STATE_ERROR;
   }
   if (context->corrupted)
   {
-    return context->corrupted;
+    return SHA1_RESULT_STATE_ERROR;
   }
   while (length-- && ! context->corrupted)
   {
