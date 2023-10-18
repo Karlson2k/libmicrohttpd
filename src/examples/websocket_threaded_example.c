Karlson2k/libmicrohttpd

commit beb7fa828c59c31d4a5178b82121435f2069b181

fix transmission stall issue with upgraded TLS connections reported by Nguyen Xuan Viet on the mailinglist

src/examples/websocket_threaded_example.c
@@ -36,40 +36,40 @@
   "<title>WebSocket chat</title>\n"                                           \
   "<script>\n"                                                                \
   "document.addEventListener('DOMContentLoaded', function() {\n"              \
-  "  const ws = new WebSocket('ws://' + window.location.host);\n"             \
-  "  const btn = document.getElementById('send');\n"                          \
-  "  const msg = document.getElementById('msg');\n"                           \
-  "  const log = document.getElementById('log');\n"                           \
-  "  ws.onopen = function() {\n"                                              \
-  "    log.value += 'Connected\\n';\n"                                        \
-  "  };\n"                                                                    \
-  "  ws.onclose = function() {\n"                                             \
-  "    log.value += 'Disconnected\\n';\n"                                     \
-  "  };\n"                                                                    \
-  "  ws.onmessage = function(ev) {\n"                                         \
-  "    log.value += ev.data + '\\n';\n"                                       \
-  "  };\n"                                                                    \
-  "  btn.onclick = function() {\n"                                            \
-  "    log.value += '<You>: ' + msg.value + '\\n';\n"                         \
-  "    ws.send(msg.value);\n"                                                 \
-  "  };\n"                                                                    \
-  "  msg.onkeyup = function(ev) {\n"                                          \
-  "    if (ev.keyCode === 13) {\n"                                            \
-  "      ev.preventDefault();\n"                                              \
-  "      ev.stopPropagation();\n"                                             \
-  "      btn.click();\n"                                                      \
-  "      msg.value = '';\n"                                                   \
-  "    }\n"                                                                   \
-  "  };\n"                                                                    \
-  "});\n"                                                                     \
-  "</script>\n"                                                               \
-  "</head>\n"                                                                 \
-  "<body>\n"                                                                  \
-  "<input type='text' id='msg' autofocus/>\n"                                 \
-  "<input type='button' id='send' value='Send' /><br /><br />\n"              \
-  "<textarea id='log' rows='20' cols='28'></textarea>\n"                      \
-  "</body>\n"                                                                 \
-  "</html>"
+  "  const ws = new WebSocket('ws:// ' + window.location.host);\n"             \
+                                  //   "  const btn = document.getElementById('send');\n"                          \
+                                  //   "  const msg = document.getElementById('msg');\n"                           \
+                                  //   "  const log = document.getElementById('log');\n"                           \
+                                  //   "  ws.onopen = function() {\n"                                              \
+                                  //   "    log.value += 'Connected\\n';\n"                                        \
+                                  //   "  };\n"                                                                    \
+                                  //   "  ws.onclose = function() {\n"                                             \
+                                  //   "    log.value += 'Disconnected\\n';\n"                                     \
+                                  //   "  };\n"                                                                    \
+                                  //   "  ws.onmessage = function(ev) {\n"                                         \
+                                  //   "    log.value += ev.data + '\\n';\n"                                       \
+                                  //   "  };\n"                                                                    \
+                                  //   "  btn.onclick = function() {\n"                                            \
+                                  //   "    log.value += '<You>: ' + msg.value + '\\n';\n"                         \
+                                  //   "    ws.send(msg.value);\n"                                                 \
+                                  //   "  };\n"                                                                    \
+                                  //   "  msg.onkeyup = function(ev) {\n"                                          \
+                                  //   "    if (ev.keyCode === 13) {\n"                                            \
+                                  //   "      ev.preventDefault();\n"                                              \
+                                  //   "      ev.stopPropagation();\n"                                             \
+                                  //   "      btn.click();\n"                                                      \
+                                  //   "      msg.value = '';\n"                                                   \
+                                  //   "    }\n"                                                                   \
+                                  //   "  };\n"                                                                    \
+                                  //   "});\n"                                                                     \
+                                  //   "</script>\n"                                                               \
+                                  //   "</head>\n"                                                                 \
+                                  //   "<body>\n"                                                                  \
+                                  //   "<input type='text' id='msg' autofocus/>\n"                                 \
+                                  //   "<input type='button' id='send' value='Send' /><br /><br />\n"              \
+                                  //   "<textarea id='log' rows='20' cols='28'></textarea>\n"                      \
+                                  //   "</body>\n"                                                                 \
+                                  //   "</html>"
 #define BAD_REQUEST_PAGE                                                      \
   "<html>\n"                                                                  \
   "<head>\n"                                                                  \

src/examples/connection_close.c
@@ -102,10 +102,10 @@ main (int argc, char *const *argv)
     return 1;
   }
   d = MHD_start_daemon (/* MHD_USE_INTERNAL_POLLING_THREAD | MHD_USE_ERROR_LOG, */
-                        /* MHD_USE_AUTO | MHD_USE_INTERNAL_POLLING_THREAD | MHD_USE_ERROR_LOG, */
-                        /* MHD_USE_INTERNAL_POLLING_THREAD | MHD_USE_ERROR_LOG | MHD_USE_POLL, */
+    /* MHD_USE_AUTO | MHD_USE_INTERNAL_POLLING_THREAD | MHD_USE_ERROR_LOG, */
+    /* MHD_USE_INTERNAL_POLLING_THREAD | MHD_USE_ERROR_LOG | MHD_USE_POLL, */
     MHD_USE_THREAD_PER_CONNECTION | MHD_USE_INTERNAL_POLLING_THREAD
-    | MHD_USE_ERROR_LOG | MHD_USE_POLL,                                                                   // | MHD_USE_ITC,
+    | MHD_USE_ERROR_LOG | MHD_USE_POLL, // | MHD_USE_ITC,
     /* MHD_USE_THREAD_PER_CONNECTION | MHD_USE_INTERNAL_POLLING_THREAD | MHD_USE_ERROR_LOG, */
     atoi (argv[1]),
     NULL, NULL, &ahc_echo, PAGE,

src/examples/demo.c
@@ -283,11 +283,11 @@ list_directory (struct ResponseDataContext *rdc,
         snprintf (fullname, sizeof (fullname),
                   "%s/%s",
                   dirname, de->d_name))
-      continue; /* ugh, file too long? how can this be!? */
+      continue;  /* ugh, file too long? how can this be!? */
     if (0 != stat (fullname, &sbuf))
-      continue; /* ugh, failed to 'stat' */
+      continue;  /* ugh, failed to 'stat' */
     if (! S_ISREG (sbuf.st_mode))
-      continue; /* not a regular file, skip */
+      continue;  /* not a regular file, skip */
     if (rdc->off + 1024 > rdc->buf_len)
     {
       void *r;

src/examples/demo_https.c
@@ -286,11 +286,11 @@ list_directory (struct ResponseDataContext *rdc,
         snprintf (fullname, sizeof (fullname),
                   "%s/%s",
                   dirname, de->d_name))
-      continue; /* ugh, file too long? how can this be!? */
+      continue;  /* ugh, file too long? how can this be!? */
     if (0 != stat (fullname, &sbuf))
-      continue; /* ugh, failed to 'stat' */
+      continue;  /* ugh, failed to 'stat' */
     if (! S_ISREG (sbuf.st_mode))
-      continue; /* not a regular file, skip */
+      continue;  /* not a regular file, skip */
     if (rdc->off + 1024 > rdc->buf_len)
     {
       void *r;

src/examples/fileserver_example.c
@@ -69,13 +69,13 @@ ahc_echo (void *cls,
     *ptr = &aptr;
     return MHD_YES;
   }
-  *ptr = NULL;                  /* reset when done */
+  *ptr = NULL;                     /* reset when done */
   /* WARNING: direct usage of url as filename is for example only!
    * NEVER pass received data directly as parameter to file manipulation
    * functions. Always check validity of data before using.
    */
   if (NULL != strstr (url, "../")) /* Very simplified check! */
-    fd = -1; /* Do not allow usage of parent directories. */
+    fd = -1;                       /* Do not allow usage of parent directories. */
   else
     fd = open (url + 1, O_RDONLY);
   if (-1 != fd)

src/include/microhttpd.h
@@ -132,7 +132,7 @@ typedef intptr_t ssize_t;
  * Current version of the library.
  * 0x01093001 = 1.9.30-1.
  */
-#define MHD_VERSION 0x00097104
+#define MHD_VERSION 0x00097105
 
 /**
  * Operational results from MHD calls.

src/lib/connection_add.c
@@ -188,8 +188,8 @@ thread_main_handle_connection (void *data)
       }
 #endif /* HAVE_POLL */
       MHD_itc_clear_ (daemon->itc);
-      continue;     /* Check again for resume. */
-    }     /* End of "suspended" branch. */
+      continue; /* Check again for resume. */
+    }           /* End of "suspended" branch. */
 
     if (was_suspended)
     {
@@ -1047,7 +1047,7 @@ MHD_accept_connection_ (struct MHD_Daemon *daemon)
     /* This could be a common occurrence with multiple worker threads */
     if (MHD_SCKT_ERR_IS_ (err,
                           MHD_SCKT_EINVAL_))
-      return MHD_SC_DAEMON_ALREADY_SHUTDOWN;   /* can happen during shutdown, let's hope this is the cause... */
+      return MHD_SC_DAEMON_ALREADY_SHUTDOWN;  /* can happen during shutdown, let's hope this is the cause... */
     if (MHD_SCKT_ERR_IS_DISCNN_BEFORE_ACCEPT_ (err))
       return MHD_SC_ACCEPT_FAST_DISCONNECT;   /* do not print error if client just disconnected early */
     if (MHD_SCKT_ERR_IS_EAGAIN_ (err) )

src/lib/connection_options.c
@@ -90,10 +90,10 @@ MHD_update_last_activity_ (struct MHD_Connection *connection)
   struct MHD_Daemon *daemon = connection->daemon;
 
   if (0 == connection->connection_timeout)
-    return; /* Skip update of activity for connections
+    return;  /* Skip update of activity for connections
                without timeout timer. */
   if (connection->suspended)
-    return; /* no activity on suspended connections */
+    return;  /* no activity on suspended connections */
 
   connection->last_activity = MHD_monotonic_sec_counter ();
   if (MHD_TM_THREAD_PER_CONNECTION == daemon->threading_mode)

commit 5c180e3aee38949372f3416e75ace841ae514a0f

websocket_threaded_example: fix websocket url string
Remove extra space character from websocket url string.

The code was broken by beb7fa82 ("fix transmission stall issue with upgraded
TLS connections reported by Nguyen Xuan Viet on the mailinglist").

src/examples/websocket_threaded_example.c
@@ -39,7 +39,7 @@
   "<title>WebSocket chat</title>\n"                                           \
   "<script>\n"                                                                \
   "document.addEventListener('DOMContentLoaded', function() {\n"              \
-  "  const ws = new WebSocket('ws:/" "/ ' + window.location.host);\n"         \
+  "  const ws = new WebSocket('ws:/" "/' + window.location.host);\n"         \
   "  const btn = document.getElementById('send');\n"                          \
   "  const msg = document.getElementById('msg');\n"                           \
   "  const log = document.getElementById('log');\n"                           \