6179e15e787386e74344306b0b437f195bd10585
Evgeny Grin (Karlson2k) <k2k@narod.ru>
2024-04-04 01:06:47 +0200

    script updates

--- scripts/d_options.sh
+++ scripts/d_options.sh
@@ -52,6 +52,17 @@ if [[ "false" ]] ; then : ; else
   exit 1
 fi
 
+if [[ $'\n' = '
+' ]] ; then : ; else
+  echo "Error: ANSI-C quoting support is required" >&2
+  exit 1
+fi
+
+if [[ "abc" =~ 'b' ]] && [[ "xyz" =~ [x-z]{3} ]] ; then : ; else
+  echo "Error: Regular expression match support is required" >&2
+  exit 1
+fi
+
 test_var="abc ABC Abc"
 if test "${test_var^}" = "Abc ABC Abc" && test "${test_var^^}" = "ABC ABC ABC" && test "${test_var,,}" = "abc abc abc"; then : ; else
   echo "Error: Shell upper- and lowercase variable conversion support required" >&2
@@ -70,6 +81,9 @@ max_width=79
 input_rec="d_options.rec"
 tmp_rec="d_options_preproc.rec"
 
+# fixed strings
+flat_arg_descr='the value of the parameter'
+
 err_exit() {
     local msg="$1"
     local err=$2
@@ -79,13 +93,29 @@ err_exit() {
     echo "$msg" >&1
     exit $err
 }
-  
+
+# cut string an newline character
+cut_str_nl() {
+    local str="$1"
+    declare -g cut_str_nl_res=''
+    if [[ "$str" =~ $'\n' ]]; then
+        cut_str_nl_res="${str%%$'\n'*}"
+        return 0
+    fi
+    return 1
+}
+
 # cut string to given length at word boundary if possible
+# process embedded new line characters
 cut_str_word () {
     local str="$1"
     local len=$2
     declare -g cut_str_word_res=''
-    [[ $len -eq 0 ]] && return 1
+    [[ $len -le 0 ]] && return 1
+    if cut_str_nl "${str:0:$(( len + 1 ))}"; then
+        cut_str_word_res="${cut_str_nl_res}"
+        return 0
+    fi
     if [[ ${#str} -le $len ]]; then
         cut_str_word_res="${str}"
         return 0
@@ -99,16 +129,17 @@ cut_str_word () {
     return 0
 }
 
-format_param_descr() {
+format_doxy() {
     local prefix1="$1" # first line prefix
     local desc="$2"
-    local width="$4"
     local prefix2="$3" # prefix on all other lines
+    local width="$4"
     local tmp_str
-    declare -g format_param_descr_res=''
+    declare -g format_doxy_res=''
     [[ -z $width ]] && width=$max_width
     prefix1="${prefix1%"${prefix1##*[! ]}"} " # force single trailing space
     [[ -z $prefix2 ]] && prefix2="$prefix1"
+    [[ ${#prefix1} -ge $width ]] && err_exit "Too long prefix ('${prefix1}') for width $width."
     desc="${desc#"${desc%%[! ]*}"}"
     desc="${desc%"${desc##*[! ]}"}" # trim desc
     local width_r=$(( width - ${#prefix1} ))
@@ -116,22 +147,39 @@ format_param_descr() {
     prefix2="
 ${prefix2}${tmp_str:${#prefix2}}"
     cut_str_word "$desc" $width_r || return 1
-    format_param_descr_res="${prefix1}${cut_str_word_res}"
+    format_doxy_res="${prefix1}${cut_str_word_res}"
     desc="${desc:${#cut_str_word_res}}"
     desc="${desc#"${desc%%[! ]*}"}" # trim leading spaces
+    desc="${desc#$'\n'}" # remove leading newline character
     while [[ -n "$desc" ]]; do
         cut_str_word "$desc" $width_r || return 1
-        format_param_descr_res+="${prefix2}${cut_str_word_res}"
+        format_doxy_res+="${prefix2}${cut_str_word_res}"
         desc="${desc:${#cut_str_word_res}}"
+        desc="${desc#"${desc%%[! ]*}"}" # trim leading spaces
+        desc="${desc#$'\n'}" # remove leading newline character
     done
     return 0
 }
+
+def_name_for_type() {
+    case $1 in
+        'enum MHD_Bool') echo -n "bool_val";;
+        'unsigned int') echo -n "uint_val";;
+        'uint_fast64_t') echo -n "uint64_val";;
+        'uint_fast32_t') echo -n "uint32_val";;
+        'uint_fast16_t') echo -n "uint16_val";;
+        'size_t') echo -n "sizet_val";;
+        *) printf '%s' "${1// /_}_val"
+    esac
+}
+
 
 cat << _EOF_ > "$tmp_rec"
 %rec: MHD_Option_preproc
 %mandatory: Value
 %mandatory: Name
 %type: Value int
+%sort: Value
 
 _EOF_
 
@@ -143,19 +191,18 @@ do
       echo "The 'Name' field is empty for 'Value=$N'" >&2
       exit 2
     fi
-    echo $N: $NAME
+    echo "$N: ${NAME^^}"
     COMMENT=$(recsel -t MHD_Option -P Comment -e "Value=$N" "$input_rec")
     if [[ -z $COMMENT ]]; then
       echo "The 'Comment' field is empty for '$NAME' ('Value=$N')" >&2
       exit 2
     fi
     TYPE=$(recsel -t MHD_Option -P Type -e "Value=$N" "$input_rec")
-    EComment="" # The doxy comment for enum value
+    EComment="" # The initial part of doxy comment for the enum value
     EName=""    # The final part of the name of the enum value
-    UComment="" # The doxy comment for the union member
     UName=""    # The final part of the name of the union member
     UType=""    # The type of the union member
-    SComment="" # The doxy comment for the value set macro and set function
+    SComment="" # The doxy comment for the set macro/function
     SName=""    # The final part of the name of the set macro/function
     MArguments=""   # The arguments for the macro
     CLBody=""   # The Compound Literal body (for the set macro)
@@ -173,80 +220,130 @@ do
     UName="${clean_name// /_}" # lowercase '_'-joined
     
     SName="${EName}" # Uppercase '_'-joined
-
-    [[ -z $TYPE ]] && nested='no'
     
-    arg_names=( )
-    arg_types=( )
-    arg_descr=( )
-    nest_membr=( )
+    format_doxy '   * ' "$COMMENT" || err_exit
+    EComment="$format_doxy_res"
+    
+    format_doxy ' * ' "$COMMENT" || err_exit
+    SComment="$format_doxy_res"
+    
+    # Read option parameters
+    ARGS=( )
+    DESCRS=( )
+    MEMBERS=( )
     M=1
     while
         ARGM=$(recsel -t MHD_Option -P Argument${M} -e "Value=$N" "$input_rec")
         [[ -n $ARGM ]]
     do
-        DESCRM=$(recsel -t MHD_Option -P Description${M} -e "Value=$N" "$input_rec")
-        if [[ -z $DESCRM ]]; then
-            echo "Empty Description${M} for argument \"$ARGM\" for '$NAME' ('Value=$N')" >&2
-            exit 2
-        fi
-        MEMBRM=$(recsel -t MHD_Option -P Member${M} -e "Value=$N" "$input_rec")
-        
-        arg_name="${ARGM##* }"
-        arg_name="${arg_name#\*}"
-        arg_type="${ARGM%${arg_name}}"
-        arg_type="${arg_type% }"
-        
-        if [[ -z $arg_type ]]; then
-            if [[ $M -eq 1 ]]; then
-                if [[ -z $TYPE ]]; then
-                    echo "No argument type for $NAME ('Value=$N')" >&2
-                    exit 2
-                else
-                    arg_type="$TYPE"
-                    nested='no'
-                fi
-            else
-                echo "No argument type found in 'Argument${M}' (\"$ARGM\") for '$NAME' ('Value=$N')" >&2
-                exit 2
-            fi
-        else
-            if [[ -z $TYPE ]]; then
-                if [[ $M -eq 1 ]]; then
-                    TYPE="$arg_type"
-                    nested='no'
-                else
-                    echo "Empty 'Type' for '$NAME' ('Value=$N') with multiple parameters" >&2
-                    exit 2
-                fi
-            else
-                if [[ $TYPE = $arg_type ]]; then
-                    if [[ $M -eq 1 ]]; then
-                        nested='no'
-                    else
-                        echo "The same 'Type' and type for in 'Argument${M}' (\"$ARGM\") used for '$NAME' ('Value=$N')" >&2
-                        exit 2
-                    fi
-                else
-                    nested='yes'
-                fi
-            fi
-        fi
-        [[ $nested = 'maybe' ]] && err_exit "Internal error"
-        if [[ $M -gt 1 ]]; then
-            if [[ $nested = 'no' ]]; then
-                echo "The 'Argument${M}' (\"$ARGM\") is specified for flat (non-nested) '$NAME' ('Value=$N')" >&2
-                exit 2
-            elif [[ $nested = 'maybe' ]]; then
-                nested='yes'
-            fi
-        fi
+        ARGS[$M]="$ARGM"
+        DESCRS[$M]="$(recsel -t MHD_Option -P Description${M} -e "Value=$N" "$input_rec")"
+        MEMBERS[$M]="$(recsel -t MHD_Option -P Member${M} -e "Value=$N" "$input_rec")"
         (( M++ ))
     done
-    if (( M < 2 )); then
+    
+    # Basic data checks
+    (( M - 1 == ${#ARGS[@]} )) || err_exit
+    
+    if [[ ${#ARGS[@]} -eq 0 ]]; then
         [[ -z $TYPE ]] && err_exit "No 'Argument1' is specified for '$NAME' ('Value=$N') without 'Type'" >&2
-        [[ $nested = 'yes' ]]  && err_exit "No 'Argument1' is specified for non-flat (nested) '$NAME' ('Value=$N')" >&2
+        nested='no'
+        ARGS[1]=''
+        DESCRS[1]="$flat_arg_descr"
+        MEMBERS[1]=''
+    elif [[ ${#ARGS[@]} -eq 1 ]]; then
+        nested='no'
+    else
+        nested='yes'
+        [[ -z $TYPE ]] && err_exit "No 'Type' is specified for non-flat (nested, with multiple parameters) '$NAME' ('Value=$N')" >&2
     fi
+    
+    # Process option parameters
+    for (( M=1 ; M <= ${#ARGS[@]} ; M++ )) ; do
+        
+        arg_name='' # The name of the current argument
+        arg_type='' # The type of the data of the current argument
+        arg_descr='' # The description of the current argument
+        nest_member='' # The name of the member of the nested structure
+        # Pre-process parameters data
+        if [[ -n ${ARGS[$M]} ]]; then
+            arg_name="${ARGS[$M]##* }"
+            arg_name="${arg_name#\*}"
+            arg_type="${ARGS[$M]%${arg_name}}"
+            arg_type="${arg_type% }"
+        else
+            if [[ $nested = 'yes' ]]; then
+                err_exit "Empty or no 'Argument${M}' ('$arg_type') for '$NAME' ('Value=$N')"
+            else
+                [[ -z $TYPE ]] && err_exit "No 'Argument1' is specified for '$NAME' ('Value=$N') without 'Type'" >&2
+                arg_name="$(def_name_for_type "$TYPE")"
+                arg_type="$TYPE"
+            fi
+        fi
+        arg_descr="${DESCRS[$M]}"
+        nest_membr="${MEMBERS[$M]}"
+
+        [[ -z $arg_name ]] && err_exit # Should not happen
+        if [[ $nested = 'yes' ]]; then
+            # non-flat, nested
+            [[ -z $arg_type ]] && err_exit "No argument type in 'Argument${M}' ('${ARGS[$M]}') for $NAME ('Value=$N')"
+            [[ $TYPE = $arg_type ]] && \
+                err_exit "The same 'Type' and type for in 'Argument${M}' ('$arg_type') used for non-flat (nested) '$NAME' ('Value=$N')"
+            [[ -z $arg_descr ]] && \
+                err_exit "Empty or no 'Description${M}' for argument '${ARGS[$M]}' for '$NAME' ('Value=$N')"
+            [[ -z $nest_membr ]] && \
+                err_exit "Empty or no 'Member${M}' for argument '${ARGS[$M]}' for nested (non-flat) '$NAME' ('Value=$N')"
+        else
+            # flat, non-nested
+            if [[ -z $arg_type ]]; then
+                if [[ -z $TYPE ]]; then
+                    err_exit "Both 'Type' and type for in 'Argument${M}' ('${ARGS[$M]}') are empty for '$NAME' ('Value=$N')"
+                else
+                    arg_type="$TYPE"
+                fi
+            else
+                if [[ -z $TYPE ]]; then
+                    TYPE="$arg_type"
+                elif [[ $TYPE != $arg_type ]]; then
+                    err_exit "Different 'Type' ('$TYPE') and type for in 'Argument${M}' ('$arg_type') used for '$NAME' ('Value=$N')"
+                fi
+            fi
+            [[ -z $arg_descr ]] && arg_descr="$flat_arg_descr"
+            [[ -n $nest_membr ]] && \
+                err_exit "'Member${M}' is provided for non-nested (flat) '$NAME' ('Value=$N')"
+        fi
+        
+        [[ "$arg_type" =~ \*$ ]] || arg_type+=' ' # Position '*' correctly
+        
+        [[ $M -gt 1 ]] && [[ $nested = 'no' ]] && err_exit
+        
+        # Use parameters data
+        
+        format_doxy ' * @param '"$arg_name " "$arg_descr" ' * '|| err_exit
+        SComment+=$'\n'"$format_doxy_res"
+        
+        [[ $M -gt 1 ]] && MArguments+=','
+        MArguments+="$arg_name"
+        
+        if [[ $nested = 'yes' ]]; then
+            [[ $M -gt 1 ]] && SFArguments+=','
+            SFArguments+=$'\n'"  ${arg_type}$arg_name"
+        else
+            SFArguments+="${arg_type}$arg_name"
+        fi
+        
+        #[[ $M -gt 1 ]] && CLBody+=', \'$'\n'"    "
+        [[ $M -gt 1 ]] && CLBody+=', \##removeme##'$'\n'"    " # '##removeme##' is a workaround for requtils bug
+        CLBody+=".val.v_${UName}"
+        [[ $nested = 'yes' ]] && CLBody+=".${nest_membr}"
+        CLBody+=" = ($arg_name)"
+
+        [[ $M -gt 1 ]] && SFBody+=$'\n'"  "
+        SFBody+="opt.val.v_${UName}"
+        [[ $nested = 'yes' ]] && SFBody+=".${nest_membr}"
+        SFBody+=" = ${arg_name};"
+        
+    done
     UType="$TYPE"
     
     recins -t MHD_Option_preproc \
@@ -254,7 +351,6 @@ do
         -f Value -v "$N" \
         -f EComment -v "$EComment" \
         -f EName -v "$EName" \
-        -f UComment -v "$UComment" \
         -f UName -v "$UName" \
         -f UType -v "$UType" \
         -f SComment -v "$SComment" \
@@ -270,7 +366,7 @@ echo "finished."
 echo "Generating output files..."
 recfmt -f d_options_enum.template <"$tmp_rec" > enum_insert.h
 recfmt -f d_options_union.template <"$tmp_rec" > union_insert.h
-recfmt -f d_options_macro.template <"$tmp_rec" > macro_insert.h
+recfmt -f d_options_macro.template <"$tmp_rec" | ${SED-sed} -e 's/##removeme##//g' - > macro_insert.h
 recfmt -f d_options_func.template <"$tmp_rec" > func_insert.h
 # rm "$tmp_rec"
 echo "finished."
