9a42653c19ff9202511f1c7a3b9c7346c0a09d71
Evgeny Grin (Karlson2k) <k2k@narod.ru>
2024-05-16 15:54:25 +0200

    compat_calloc.{h,c}: implementation of calloc() replacement

---
+++ src/mhd2/compat_calloc.c
@@ -0,0 +1,63 @@
+/*
+  This file is part of GNU libmicrohttpd
+  Copyright (C) 2014-2024 Karlson2k (Evgeny Grin)
+
+  GNU libmicrohttpd is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  GNU libmicrohttpd is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+
+/**
+ * @file src/mhd2/compat_calloc.c
+ * @brief  The implementation of the calloc() replacement
+ * @author Karlson2k (Evgeny Grin)
+ */
+
+#include "compat_calloc.h"
+#ifndef HAVE_CALLOC
+
+#include <string.h> /* for memset() */
+#include "sys_malloc.h"
+
+
+#ifdef __has_builtin
+#  if __has_builtin (__builtin_mul_overflow)
+#    define MHD_HAVE_MUL_OVERFLOW 1
+#  endif
+#elif defined(__GNUC__) && __GNUC__ + 0 >= 5
+#  define MHD_HAVE_MUL_OVERFLOW 1
+#endif /* __GNUC__ >= 5 */
+
+MHD_INTERNAL void *
+MHD_calloc_ (size_t nelem, size_t elsize)
+{
+  size_t alloc_size;
+  void *ptr;
+#ifdef MHD_HAVE_MUL_OVERFLOW
+  if (__builtin_mul_overflow (nelem, elsize, &alloc_size) || (0 == alloc_size))
+    return NULL;
+#else  /* ! MHD_HAVE_MUL_OVERFLOW */
+  alloc_size = nelem * elsize;
+  if ((0 == alloc_size) || (elsize != alloc_size / nelem))
+    return NULL;
+#endif /* ! MHD_HAVE_MUL_OVERFLOW */
+  ptr = malloc (alloc_size);
+  if (NULL == ptr)
+    return NULL;
+  memset (ptr, 0, alloc_size);
+  return ptr;
+}
+
+
+#endif /* ! HAVE_CALLOC */
---
+++ src/mhd2/compat_calloc.h
@@ -0,0 +1,65 @@
+/*
+  This file is part of GNU libmicrohttpd
+  Copyright (C) 2024 Evgeny Grin (Karlson2k)
+
+  GNU libmicrohttpd is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  GNU libmicrohttpd is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+
+/**
+ * @file src/mhd2/compat_calloc.h
+ * @brief  The header for the calloc() or the calloc() replacement declarations
+ * @author Karlson2k (Evgeny Grin)
+ */
+
+#ifndef MHD_COMPAT_CALLOC_H
+#define MHD_COMPAT_CALLOC_H 1
+
+#include "mhd_sys_options.h"
+
+#ifdef HAVE_CALLOC
+#  if defined(HAVE_STDLIB_H)
+#    include <stdlib.h>
+#  elif defined(HAVE_MALLOC_H)
+#    include <malloc.h>
+#  else
+/* Try some set of headers, hoping the right header is included */
+#    if defined(HAVE_UNISTD_H)
+#      include <unistd.h>
+#    endif
+#    include <stdio.h>
+#    include <string.h>
+#  endif
+
+#  define MHD_calloc_ calloc
+#else
+
+#  include "sys_base_types.h" /* for size_t, NULL */
+
+
+/**
+ * Allocate memory for an array of @a nelem objects of @a elsize size and
+ * initialise all bytes to zero in the allocated memory area.
+ * @param nelem the number of elements to allocate
+ * @param elsize the size of single element
+ * @return the pointer to allocated memory area on success,
+ *         the NULL pointer on failure.
+ */
+MHD_INTERNAL void *
+MHD_calloc_ (size_t nelem, size_t elsize);
+
+#endif /* ! HAVE_CALLOC */
+
+#endif /* ! MHD_COMPAT_CALLOC_H */
