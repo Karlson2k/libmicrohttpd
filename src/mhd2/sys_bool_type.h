758a58e8f6d2972e32aff801477bd2a44afc434c
Evgeny Grin (Karlson2k) <k2k@narod.ru>
2024-05-16 15:03:26 +0200

    sys_bool_type.h: new internal header

---
+++ src/mhd2/sys_bool_type.h
@@ -0,0 +1,38 @@
+/*
+  This file is part of GNU libmicrohttpd
+  Copyright (C) 2024 Evgeny Grin (Karlson2k)
+
+  GNU libmicrohttpd is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  GNU libmicrohttpd is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+
+/**
+ * @file src/mhd2/sys_bool_type.h
+ * @brief  The header for the system 'bool' type
+ * @author Karlson2k (Evgeny Grin)
+ *
+ * This header provides 'bool' type and 'true' and 'false' values.
+ */
+
+#ifndef MHD_BASE_BOOL_TYPE_H
+#define MHD_BASE_BOOL_TYPE_H 1
+
+#include "mhd_sys_options.h"
+
+#ifdef HAVE_STDBOOL_H
+#  include <stdbool.h>
+#endif
+
+#endif /* ! MHD_BASE_BOOL_TYPE_H */
