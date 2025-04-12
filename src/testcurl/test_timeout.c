c372caeb3cc30a996338d84d4ea45aa9cc5dbfab
Evgeny Grin (Karlson2k) <k2k@narod.ru>
2025-04-12 19:45:47 +0300

    test_timeout: more detailed libcurl error report

--- src/testcurl/test_timeout.c
+++ src/testcurl/test_timeout.c
@@ -392,8 +392,12 @@ testWithTimeout (void)
       }
     }
     else
-      /* curl had the timeout first */
+    {
+      fprintf (stderr, "libcurl reported error: %s (%u)\n",
+               curl_easy_strerror (errornum),
+               errornum);
       return 32;
+    }
   }
   curl_easy_cleanup (c);
   MHD_stop_daemon (d);
